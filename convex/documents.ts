af8e6e33479042b4229c5157b3345aebf6d3e103
Deepanshu Mishra <d4deepanshu723@gmail.com>
2024-12-17 23:48:51 +0530

    checkpoint 1

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,19 +1,34 @@
-import { v } from "convex/values";
+import { ConvexError, v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const getDocuments = query({
-    async handler(ctx){
-        return await ctx.db.query('documents').collect();
+  async handler(ctx) {
+    const userId = await ctx.auth.getUserIdentity();
+    const tokenIdentifier = userId?.tokenIdentifier;
+
+    if (!tokenIdentifier) {
+        return [];
     }
-})
+    return await ctx.db
+      .query("documents")
+      .withIndex("by_token_identifier", (q) => q.eq("tokenIdentifier", tokenIdentifier)).collect();
+  },
+});
 
 export const createDocument = mutation({
   args: {
     title: v.string(),
   },
   async handler(ctx, args_0) {
+    const userId = await ctx.auth.getUserIdentity();
+    const tokenIdentifier = userId?.tokenIdentifier;
+
+    if (!tokenIdentifier) {
+      throw new ConvexError("Unauthorized");
+    }
     await ctx.db.insert("documents", {
       title: args_0.title,
+      tokenIdentifier: tokenIdentifier,
     });
   },
 });
