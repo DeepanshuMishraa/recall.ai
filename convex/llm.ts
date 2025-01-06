37ca81020efdb4e5a13e2b8d2882da064ad89b75
Deepanshu <d4deepanshu723@gmail.com>
2025-01-06 23:55:16 +0530

    fix

--- convex/llm.ts
+++ convex/llm.ts
@@ -8,13 +8,9 @@ export const getGroqChatCompletion = async (text: string) => {
   return groq.chat.completions.create({
     messages: [
       {
-        role: "system",
-        content: `Here is a text file ${text}`,
-      },
-      {
         role: "user",
         content:
-          "I would like you to go through this text file and answer the queries asked based on the content",
+          `I would like you to go through this text file ${text} and answer the queries asked based on the content`,
       },
     ],
     model: "llama-3.3-70b-versatile",
